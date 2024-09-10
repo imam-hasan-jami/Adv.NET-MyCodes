d82470f2b440d18fcb9897ffba26bd197942ff48
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-09-10 21:11:22 +0600

    initial commit

---
+++ ProductWebAPI/BLL/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using DAL.EF.TableModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PId { get; set; }
+    }
+}
